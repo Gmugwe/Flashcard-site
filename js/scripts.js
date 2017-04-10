5bbbb4f57c641a4b5c2720e486a56be39f0ca2e6
gmugwe <ggmugwe@gmail.com>
2017-04-10 09:13:56 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+     $("#one").click(function() {
+       $("#two").show();
+
+     });
+   });
+    $(document).ready(function() {
+        $(".clickable1").click(function() {
+          $("#one").toggle();
+           $("#two").toggle();
+         });
+         });
+
+     $(document).ready(function() {
+        $("#three").click(function() {
+             $("#four").show();
+
+              });
+            });
+    $(document).ready(function() {
+          $(".clickable2").click(function() {
+          $("#three").toggle();
+          $("#four").toggle();
+          });
+            });
+
+      $(document).ready(function() {
+               $("#five").click(function() {
+                $("#six").show();
+
+                     });
+                   });
+       $(document).ready(function() {
+                 $(".clickable3").click(function() {
+                 $("#five").toggle();
+                 $("#six").toggle();
+                 });
+                   });
+         $(document).ready(function() {
+                 $("#seven").click(function() {
+                 $("#eight").show();
+
+                                  });
+                                });
+            $(document).ready(function() {
+                    $(".clickable4").click(function() {
+                    $("#seven").toggle();
+                    $("#eight").toggle();
+                              });
+                                });
+              $(document).ready(function() {
+                  $("#nine").click(function() {
+                 $("#ten").show();
+
+                  });
+                     });
+              $(document).ready(function() {
+                   $(".clickable5").click(function() {
+                   $("#nine").toggle();
+                   $("#ten").toggle();
+                        });
+                          });
+
+                $(document).ready(function() {
+                      $("#eleven").click(function() {
+                      $("#twelve").show();
+
+                        });
+                         });
+                  $(document).ready(function() {
+                       $(".clickable6").click(function() {
+                       $("#eleven").toggle();
+                       $("#twelve").toggle();
+
+                          });
+                          });
+                    $(document).ready(function() {
+                        $("#thirteen").click(function() {
+                        $("#fourteen").show();
+
+                           });
+                           });
+                      $(document).ready(function() {
+                           $(".clickable7").click(function() {
+                            $("#thirteen").toggle();
+                             $("#fourteen").toggle();
+                           });
+                           });
